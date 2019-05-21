a6326928e55063b0fa3a7fcba0d6b510fec4cbff
ALEXIS RAJIS <arajis06@gmail.com>
2019-05-21 15:16:33 -0400

    adding Q&A array

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+    var triviaQandA = [
+        {
+            question: "Who is the leader of the Justice League?", 
+            choice: ["Flash", "Batman", "Green Lantern", "Superman", "Aquaman"],
+            answer: "Superman",
+            photo: "assets/images/pupusas.jpg"
+         },
+         {
+            question: "Who sacrificed themselves to save the sun?", 
+            choice: ["Superman", "Batman", "Starboy", "Green Lantern"],
+            answer: "Starboy",
+            photo: "assets/images/mtdew.gif"
+         } 
