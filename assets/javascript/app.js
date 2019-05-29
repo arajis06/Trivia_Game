c1b49bf9bfbfe8241703ae98868d57486e4b7bd5
ALEXIS RAJIS <arajis06@gmail.com>
2019-05-29 14:35:00 -0400

    update timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -76,7 +76,7 @@
     var correctCount = 0;
     var wrongCount = 0;
     var unanswerCount = 0;
-    var timer = 20;
+    var timer = 10;
     var intervalId;
     var qIndex;
     var qPicked;
@@ -93,8 +93,8 @@
     //THIS STARTS THE GAME ON USER CLICK
     $("#start").on("click", function () {
         $(this).hide(); //THIS HIDES THE START BUTTON AFTER CLICKED
-        displayQuestion(); //DISPLAYS QUESTIONS AFTER CLICK
         runTimer();  //RUNS TIMER AFTER CLICK
+        displayQuestion(); //DISPLAYS QUESTIONS AFTER CLICK
         for(var i = 0; i < triviaOptions.length; i++) { //PICKS A RANDOM QUESTION TO BE DISPLAYED AFTER CLICK
         holder.push(triviaOptions[i]); //MOVES SELECTED RANDOM NUMBER INTO THE OBLECT HOLDER
     }
@@ -108,7 +108,7 @@
     }
     //TIMER COUNTDOWN
     function decrement() {
-        $("#time-left").html("<h3>Time remaining: " + timer + "</h3>");
+        $("#time-left").html("<h4>Time remaining: " + timer + "</h4>");
         timer --;
     
         //STOP TIMER WHEN REACH 0
@@ -116,7 +116,7 @@
             unanswerCount++;
             stop();
             $("#answer-section").html("<p>Time is up! The correct answer is: " + qPicked.choice[qPicked.answer] + "</p>");
-            hidepicture();
+            displayphoto();
         }	
     }
     
@@ -134,7 +134,7 @@
             
         
             //ITERATES THROUGH ANSWER ARRAY AND DISPLAY  
-            $("#question-section").html("<h3>" + qPicked.question + "</h3>");
+            $("#question-section").html("<h4>" + qPicked.question + "</h4>");
             for(var i = 0; i < qPicked.choice.length; i++) {
                
                 var userChoice = $("<button>");
@@ -158,37 +158,38 @@
             correctCount++;
             userPick="";
             $("#answer-section").html("<p>Correct!</p>");
-            hidepicture();
+            displayphoto();
         //DISPLAYS CORRECT ANSWER
         } else {
             stop();
             wrongCount++;
             userPick="";
             $("#answer-section").html("<p>Wrong! The correct answer is: " + qPicked.choice[qPicked.answer] + "</p>");
-            hidepicture();
+            displayphoto();
         }
     })
     }
 //FUNCTION==================================================================
 //THIS HIDES PICTIURES AND SHOW SCORE RESULTS
-    function hidepicture () {
+    function displayphoto () {
         $("#answer-section").append("<img src=" + qPicked.photo + " class='images'>");
         newArray.push(qPicked);
         triviaOptions.splice(qIndex,1);
-    
+
+    // HIDES PHOTO
         var hidpic = setTimeout(function() {
             $("#answer-section").empty();
-            timer= 20;
+            timer= 10;
         //==============================
     
         var qCount = triviaOptions.length;
         //DISPLAYS RESULTS FROM GAME AND DISPLAY IN THE INDEX.HTML FILE
         if ((wrongCount + correctCount + unanswerCount) === qCount) {
             $("#question-section").empty();
-            $("#question-section").html("<h3>Game Over!  Here's how you did: </h3>");
-            $("#answer-section").append("<h4> Correct: " + correctCount + "</h4>" );
-            $("#answer-section").append("<h4> Incorrect: " + wrongCount + "</h4>" );
-            $("#answer-section").append("<h4> Unanswered: " + unanswerCount + "</h4>" );
+            $("#question-section").html("<h4>Game Over! Scores: </h4>");
+            $("#answer-section").append("<h5> Correct: " + correctCount + "</h5>" );
+            $("#answer-section").append("<h5> Incorrect: " + wrongCount + "</h5>" );
+            $("#answer-section").append("<h5> Unanswered: " + unanswerCount + "</h5>" );
             $("#reset").show();
             correctCount = 0;
             wrongCount = 0;
@@ -199,7 +200,7 @@
             displayQuestion();
     
         }
-        }, 3000);
+        }, 1000);//THIS SETS THE TIME BETWEEN QUESTIONS DELAY
     
     
     }
